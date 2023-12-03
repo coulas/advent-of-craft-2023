35eaa5207c126aa76dd9b424e6d82388ea0e1d37
Nicolas <nicolas.fedou@permasoft.io>
2023-12-03 01:11:05 +0100

    Don't extract method to document if it make algorithm harder to understand (misplaced cleverness, pattern abuse, etc.)

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -9,31 +9,24 @@ public class FizzBuzz {
             throw new OutOfRangeException(input+" is not between 0 included and 100 excluded");
         }
         String result = ""; // start with an empty result
-        result += appendWhenFizzing(input); // Note that I prefer when those 3 methods are inlined
-        result += appendWhenBuzzing(input); // when code grows, it may end up like that incrementally build result in business methods
-        result += appendSelfWhenEmpty(input, result); // but here inlined solution is less "clever", and easy to understand
+        if (isFizzing(input)) {
+            result += "Fizz";
+        }
+        if (isBuzzing(input)) {
+            result += "Buzz";
+        }
+        if (result.isBlank()) {
+            result = input.toString();
+        }
         return result;
     }
 
-    private static String appendWhenFizzing(Integer input) {
-        if (input % 3 == 0) {
-            return "Fizz";
-        }
-        return "";
+    private static boolean isFizzing(Integer input) {
+        return input % 3 == 0;
     }
 
-    private static String appendWhenBuzzing(Integer input) {
-        if (input % 5 == 0) {
-            return "Buzz";
-        }
-        return "";
-    }
-
-    private static String appendSelfWhenEmpty(Integer input, String result) {
-        if (result.isBlank()) {
-            return input.toString();
-        }
-        return "";
+    private static boolean isBuzzing(Integer input) {
+        return input % 5 == 0;
     }
 
     private static boolean isOutOfFizzBuzzRange(Integer input) {
